a89602f3a50fe6b4e5439e0067206b2e940cc59f
Tim <timich1995@gmail.com>
2021-11-10 17:41:15 +0500

    ImagePreviewModal added to ImagesPreview component.

---
+++ src/components/ImagesPreview/components/ImagePreviewModal/index.tsx
@@ -0,0 +1,57 @@
+import React, { FC } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { fonts } from '../../../../theme';
+
+import { ImageAsset } from '../../../../services/data';
+import { Button } from '../../../../ui-kit/Buttons';
+import Modal, { ModalProps } from '../../../../ui-kit/Modal';
+
+interface ConfirmImageModalProps extends ModalProps {
+  image: ImageAsset;
+  isBase64?: boolean;
+  onClose: () => void;
+}
+
+const ConfirmImageModal: FC<ConfirmImageModalProps> = ({
+  image,
+  isBase64 = false,
+  onClose = () => {},
+  ...otherProps
+}) => {
+  return (
+    <Modal showConfirm={false} {...otherProps}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{
+            uri: isBase64 ? `data:image/jpeg;base64,${image.uri}` : image.uri,
+          }}
+          style={{
+            height: 300,
+          }}
+          resizeMode="contain"
+        />
+      </View>
+
+      <View style={styles.commentContainer}>
+        <Text style={fonts.bodyMedium}>{image?.comment}</Text>
+      </View>
+
+      <View style={styles.controlsContainer}>
+        <Button onPress={onClose}>Закрыть</Button>
+      </View>
+    </Modal>
+  );
+};
+
+export default ConfirmImageModal;
+
+const styles = StyleSheet.create({
+  imageContainer: {},
+  commentContainer: {
+    marginVertical: 20,
+    paddingHorizontal: 20,
+  },
+  controlsContainer: {
+    paddingHorizontal: 10,
+  },
+});
--- src/components/ImagesPreview/index.tsx
+++ src/components/ImagesPreview/index.tsx
@@ -1,8 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Image, StyleProp, StyleSheet, Text, View, ViewStyle } from 'react-native';
 import { fonts, layout } from '../../theme';
 
 import { ImageAsset } from '../../services/data';
+import { TouchableRipple } from 'react-native-paper';
+import { ImagePreviewModal } from './components';
 
 export interface ImagesPreviewProps {
   title?: string;
@@ -21,35 +23,49 @@ const ImagesPreview: FC<ImagesPreviewProps> = ({
   showCommentsLabel = true,
   isBase64 = false,
 }) => {
+  const [preview, setPreview] = useState(false);
+  const [previewImage, setPreviewImage] = useState<ImageAsset>(null);
+
+  const handleOpenPreview = (image: ImageAsset) => {
+    setPreviewImage(image);
+    setPreview(true);
+  };
+
   return (
     <View style={[styles.container, containerStyle]}>
       {title && <Text style={styles.title}>{title}</Text>}
 
       <View style={styles.imagesContainer}>
-        {items.map((file: ImageAsset) => (
-          <View style={{ marginRight: 20 }} key={file.id}>
-            <Image
-              source={{
-                uri: isBase64 ? `data:image/jpeg;base64,${file.uri}` : file.uri,
-              }}
-              style={{
-                width: 100,
-                height: 100,
-                marginRight: 10,
-                marginBottom: 10,
-              }}
-              resizeMode="cover"
-            />
+        {items.map((image: ImageAsset) => (
+          <View style={{ marginRight: 20 }} key={image.id}>
+            <TouchableRipple
+              onPress={() => handleOpenPreview(image)}
+              style={{ marginRight: 10, marginBottom: 10 }}
+              borderless
+            >
+              <Image
+                source={{
+                  uri: isBase64 ? `data:image/jpeg;base64,${image.uri}` : image.uri,
+                }}
+                style={{
+                  width: 100,
+                  height: 100,
+                }}
+                resizeMode="cover"
+              />
+            </TouchableRipple>
 
-            {file.comment && showComments ? (
+            {image.comment && showComments ? (
               <View style={{ marginBottom: 10 }}>
                 {showCommentsLabel && <Text style={fonts.paragraphRegular}>Комментарий</Text>}
-                <Text style={fonts.paragraphSemibold}>{file.comment}</Text>
+                <Text style={fonts.paragraphSemibold}>{image.comment}</Text>
               </View>
             ) : null}
           </View>
         ))}
       </View>
+
+      <ImagePreviewModal isBase64={isBase64} visible={preview} onClose={() => setPreview(false)} image={previewImage} />
     </View>
   );
 };
